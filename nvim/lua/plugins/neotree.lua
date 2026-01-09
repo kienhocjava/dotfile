828ae16c7612bd75d5c24662bea5f33c7ddaac74
ngkien3001 <135014792+kienhocjava@users.noreply.github.com>
2026-01-10 00:13:09 +0700

    Add files via upload

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 30,
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
